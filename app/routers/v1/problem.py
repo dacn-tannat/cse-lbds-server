5ac12d852249d7de14034fd79c2a4c2e621b149c
thao-vonguyen <thao.vonguyen@hcmut.edu.vn>
2025-02-09 21:16:11 +0700

    delete invalid name

--- app/routers/v1/problem.py
+++ app/routers/v1/problem.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from fastapi import Depends
 from sqlalchemy.orm import Session
-from app.auth.google_auth import get_current_user
 from app.database.config import get_db
 from app.services.problem import ProblemService
 
@@ -21,16 +20,6 @@ def get_problem_by_id(id: int, db: Session = Depends(get_db)):
     """API lấy thông tin của một problem bằng id."""
     try:
         return ProblemService(db).get_by_id(id)
-    except Exception as e:
-        print(e)
-        raise e
-    
-@problemRouter.get('/protected')
-def get_problems(current_user: dict = Depends(get_current_user), db: Session = Depends(get_db)):
-    """API lấy thông tin tất cả problem trong db."""
-    print(current_user)
-    try:
-        return ProblemService(db).get_all()
     except Exception as e:
         print(e)
         raise e
