a18673d651d9abd2c02e8091174063bebb06ea18
duyanhhh1811 <duyanhlucas302@gmail.com>
2025-02-19 12:47:56 +0700

    refactor: auth service in auth router

--- app/routers/v1/auth.py
+++ app/routers/v1/auth.py
@@ -14,17 +14,17 @@ async def login_google(data: dict, db: Session = Depends(get_db)):
     if not code:
         raise HTTPException(status_code=400, detail='Code not provided')
 
-    # Get user_info from Google
-    user_info = await AuthService().get_user_info_from_google(code)
-    
-    print(user_info)
-    
-    # Create new user in case 
+    auth_service = AuthService()
     user_service = UserService(db)
+    
+    # Get user_info from Google
+    user_info = await auth_service.get_user_info_from_google(code)
+        
+    # Create new user in case 
     if user_service.get_user(user_info['sub']) is None:
         user_service.create_user(user_info)
 
     # Create access token for user
-    access_token = AuthService().create_access_token(user_info)
+    access_token = auth_service.create_access_token(user_info)
 
     return GenericResponse(data={'access_token': access_token, 'user': user_info})
