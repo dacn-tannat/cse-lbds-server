32950cb6e2a1ad92e7bf38a8571729591df00111
dasabu <129451715+dasabu@users.noreply.github.com>
2025-03-13 14:42:21 +0700

    CustomBiLSTM (#12)
    * fix: buggy position service
    
    * undo buggy position service
    
    * fix: docker compose jobe url to localhost
    
    * add CustomBiLSTM model

--- app/routers/v1/prediction.py
+++ app/routers/v1/prediction.py
@@ -14,7 +14,7 @@ from app.database.schemas.generic_response import GenericResponse
 from app.database.schemas.prediction import BugCheckRequestSchema, BugCheckType, BugPositionResponseSchema, BuggyPositionSchema
 from app.services.model import ModelService
 from app.services.buggy_position import BuggyPositionService
-from app.services.prediction.prediction import BiLSTMPredictionService, PredictionService
+from app.services.prediction.prediction import BiLSTMPredictionService, CustomBiLSTMPredictionService, PredictionService
 from app.services.source_code import SourceCodeService
 from app.services.user import UserService
 
@@ -28,10 +28,15 @@ def predict(source_code_id: int, user: dict = Depends(auth_service.get_current_u
         source_code: SourceCode = SourceCodeService(db).get_by_id(source_code_id)
         if source_code.user_id != user['sub']:
             raise HTTPException(status_code=401, detail='Cannot access this source code.')
-        user: User = UserService(db).get_user(user['sub'])
-        model_type = user.model_type
+        model_type = ast.literal_eval(ConfigRepository(db).get_by_name('ACTIVE_MODEL').value)
+        # if model_type is None or len(model_type) == 0:
+        #     model_type = 'BiLSTM'
+        # else:
+        #     model_type = random.choice(model_type)
+        # NOTE: Override model_type for testing
+        model_type = 'CustomBiLSTM'
         model: Model = ModelService(db).get_model(model_type, source_code.problem_id)
-        prediction = BiLSTMPredictionService(db, model).create_prediction(source_code)
+        prediction = CustomBiLSTMPredictionService(db, model).create_prediction(source_code)
         return GenericResponse(data=prediction)
     except Exception as e:
         print(e)
